23250a5a53cbeb78842f257181d65558ef6f87a6
Lyle Scott <Lyle.C.Scott@gmail.com>
2015-09-21 09:33:03 -0700

    setup sass, npm, gulp, initial files

---
+++ node_modules/surge/lib/middleware/index.js
@@ -0,0 +1,26 @@
+module.exports = [
+  require("./whitelist"),
+  require("./endpoint"),
+  require("./pkg"),
+  require("./version"),
+  require("./help"),
+  require("./creds"),
+  require("./whoami"),
+  require("./logout"),
+  require("./tokencheck"),
+  require("./email"),
+  require("./auth"),
+  require("./login"),
+  require("./list"),
+  require("./plus"),
+  require("./ssl"),
+  require("./teardown"),
+  require("./shorthand"),
+  require("./project"),
+  require("./size"),
+  require("./domain"),
+  require("./protocol"),
+  require("./deploy"),
+  require("./ipaddress")
+  //require("./log")
+]
