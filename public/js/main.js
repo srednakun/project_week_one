d14b5b55699cc92e0a757a5f008813abeedbd459
Lyle Scott <Lyle.C.Scott@gmail.com>
2015-09-24 11:03:57 -0700

    refactor JS table using .each loops to make code DRY

--- public/js/main.js
+++ public/js/main.js
@@ -1,113 +1,35 @@
 $(document).ready(function () {
+  var slides = ["carousel-inner-download", "carousel-inner-download-2", "carousel-inner-download-3", "carousel-inner-download-4", "carousel-inner-reviews"];
 
-  $(".carousel-inner-download").cycle({
-    fx:'scrollDown',
-    timeout: 4000,
-    speed: 1000,
+  var clickableImage = [
+    ['c1-right', 'slider-inner img'],
+    ['c1-left', 'slider-inner img'],
+    ['c1-right-2', 'slider-inner-2 img'],
+    ['c1-left-2', 'slider-inner-2 img'],
+    ['c1-right-3', 'slider-inner-3 img'],
+    ['c1-left-3', 'slider-inner-3 img'],
+  ];
+
+  $.each(slides, function(index,value) {
+      $("." + value).cycle({
+      fx:'scrollDown',
+      timeout: 4000,
+      speed: 1000,
+    });
   });
 
-  $(".carousel-inner-download-2").cycle({
-    fx:'scrollUp',
-    timeout: 4000,
-    speed: 1000,
+  $.each(clickableImage, function(index, value) {
+    $("." + value[0]).on("click", function(e) {
+      var activeImage = $(".c1-image-shown");
+      var nextImage = activeImage.next();
+
+        if(nextImage.length == 0) {
+          nextImage = $("." + value[1]).first();
+        };
+
+      activeImage.removeClass("c1-image-shown").addClass("c1-image-hidden").css("z-index",-10);;
+      nextImage.addClass("c1-image-shown").removeClass("c1-image-hidden").css("z-index",20);
+      $(".slider-inner img").not([activeImage, nextImage]).css("z-index",1);
+    });
   });
-
-  $(".carousel-inner-download-3").cycle({
-    fx:'scrollDown',
-    timeout: 4000,
-    speed: 1000,
-  });
-
-  $(".carousel-inner-download-4").cycle({
-    fx:'scrollUp',
-    timeout: 4000,
-    speed: 1000,
-  });
-
-    $(".carousel-inner-reviews").cycle({
-    fx:'scrollDown',
-    timeout: 3000,
-    speed: 1000,
-  });
-
-  $(".c1-right").on("click", function(e) {
-    var activeImage = $(".c1-image-shown");
-    var nextImage = activeImage.next();
-
-      if(nextImage.length == 0) {
-        nextImage = $(".slider-inner img").first();
-      };
-
-    activeImage.removeClass("c1-image-shown").addClass("c1-image-hidden").css("z-index",-10);;
-    nextImage.addClass("c1-image-shown").removeClass("c1-image-hidden").css("z-index",20);
-    $(".slider-inner img").not([activeImage, nextImage]).css("z-index",1);
-  });
-
-  $(".c1-left").on("click", function(e){
-    var activeImage = $(".c1-image-shown");
-    var nextImage = activeImage.prev();
-
-    if(nextImage.length == 0) {
-      nextImage = $('.slider-inner img').last();
-    };
-
-    activeImage.removeClass("c1-image-shown").addClass("c1-image-hidden").css("z-index", -10);
-    nextImage.addClass("c1-image-shown").removeClass("c1-image-hidden").css("z-index", 20);
-    $('.slider-inner img').not([activeImage, nextImage]).css("z-index", 1);
-
-  })
-    $(".c1-right-2").on("click", function(e) {
-    var activeImage = $(".c1-image-shown");
-    var nextImage = activeImage.next();
-
-      if(nextImage.length == 0) {
-        nextImage = $(".slider-inner-2 img").first();
-      };
-
-    activeImage.removeClass("c1-image-shown").addClass("c1-image-hidden").css("z-index",-10);;
-    nextImage.addClass("c1-image-shown").removeClass("c1-image-hidden").css("z-index",20);
-    $(".slider-inner img").not([activeImage, nextImage]).css("z-index",1);
-  });
-
-  $(".c1-left-2").on("click", function(e){
-    var activeImage = $(".c1-image-shown");
-    var nextImage = activeImage.prev();
-
-    if(nextImage.length == 0) {
-      nextImage = $('.slider-inner-2 img').last();
-    };
-
-    activeImage.removeClass("c1-image-shown").addClass("c1-image-hidden").css("z-index", -10);
-    nextImage.addClass("c1-image-shown").removeClass("c1-image-hidden").css("z-index", 20);
-    $('.slider-inner img').not([activeImage, nextImage]).css("z-index", 1);
-
-  });
-
-    $(".c1-right-3").on("click", function(e) {
-    var activeImage = $(".c1-image-shown");
-    var nextImage = activeImage.next();
-
-      if(nextImage.length == 0) {
-        nextImage = $(".slider-inner-3 img").first();
-      };
-
-    activeImage.removeClass("c1-image-shown").addClass("c1-image-hidden").css("z-index",-10);;
-    nextImage.addClass("c1-image-shown").removeClass("c1-image-hidden").css("z-index",20);
-    $(".slider-inner img").not([activeImage, nextImage]).css("z-index",1);
-  });
-
-  $(".c1-left-3").on("click", function(e){
-    var activeImage = $(".c1-image-shown");
-    var nextImage = activeImage.prev();
-
-    if(nextImage.length == 0) {
-      nextImage = $('.slider-inner-3 img').last();
-    };
-
-    activeImage.removeClass("c1-image-shown").addClass("c1-image-hidden").css("z-index", -10);
-    nextImage.addClass("c1-image-shown").removeClass("c1-image-hidden").css("z-index", 20);
-    $('.slider-inner img').not([activeImage, nextImage]).css("z-index", 1);
-
-  })
-
 });
